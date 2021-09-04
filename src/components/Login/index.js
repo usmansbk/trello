ceaec3a428b308f97fbefaeb8555ea02477ed54c
Usman Suleiman <usmansbk@gmail.com>
2021-09-04 18:02:40 +0100

    Style selection

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -9,12 +9,15 @@ const Login = () => {
       <Logo />
       <div className={styles.body}>
         <section className={styles.content}>
-          <form className={styles.form}>
-            <h1 className={styles.title}>Log in to Trello</h1>
-            <TextInput placeholder="Enter email" type="email" />
-            <TextInput placeholder="Enter password" type="password" />
-            <Button type="submit" value="Log in" />
-          </form>
+          <div className={styles.form}>
+            <form>
+              <h1 className={styles.title}>Log in to Trello</h1>
+              <TextInput placeholder="Enter email" type="email" />
+              <TextInput placeholder="Enter password" type="password" />
+              <Button type="submit" value="Log in" />
+            </form>
+            <div className={styles.separator}>OR</div>
+          </div>
         </section>
       </div>
     </div>
