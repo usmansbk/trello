4afa8ab0a8f4ca89076a54779267dc76d82e870a
Usman Suleiman <usmansbk@gmail.com>
2021-09-04 18:35:01 +0100

    Add google button

--- src/components/common/Button/Social.js
+++ src/components/common/Button/Social.js
@@ -1,8 +1,10 @@
 import styles from "./social.module.css";
+import google from "./google.svg";
 
 const Button = ({ text }) => {
   return (
     <button type="button" className={styles.button}>
+      <img src={google} alt="" className={styles.icon} />
       {text}
     </button>
   );
