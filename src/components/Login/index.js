40d8d680999547a6251cf240113c3e929db36b0a
Usman Suleiman <usmansbk@gmail.com>
2021-09-04 18:10:44 +0100

    Style form

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -9,7 +9,7 @@ const Login = () => {
       <Logo />
       <div className={styles.body}>
         <section className={styles.content}>
-          <div className={styles.form}>
+          <div className={styles.wrapper}>
             <form>
               <h1 className={styles.title}>Log in to Trello</h1>
               <TextInput placeholder="Enter email" type="email" />
