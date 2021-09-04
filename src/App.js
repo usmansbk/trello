d5218a2090ee74a5f48d6c5c011de88c1c045ac4
Usman Suleiman <usmansbk@gmail.com>
2021-09-04 12:21:33 +0100

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} className="App-logo" alt="" />
       <h1>Trello</h1>
     </div>
   );
