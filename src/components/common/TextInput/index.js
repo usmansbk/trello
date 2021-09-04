8f09a7d92f1015914b2df083d38af038051569e8
Usman Suleiman <usmansbk@gmail.com>
2021-09-04 14:46:26 +0100

    Style text input

--- src/components/common/TextInput/index.js
+++ src/components/common/TextInput/index.js
@@ -1,5 +1,11 @@
+import styles from "./index.module.css";
+
 const TextInput = ({ placeholder, type = "text" }) => {
-  return <input placeholder={placeholder} type={type} />;
+  return (
+    <div className={styles.container}>
+      <input placeholder={placeholder} type={type} className={styles.input} />
+    </div>
+  );
 };
 
 export default TextInput;
