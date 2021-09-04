305646222c435c8a8ce34b2aefc73c399483e9fc
Usman Suleiman <usmansbk@gmail.com>
2021-09-04 21:33:41 +0100

    Add header logo

--- src/components/Home/Header.js
+++ src/components/Home/Header.js
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
-import loading from "./loading.gif";
+import loadingLogo from "./imgs/loading.gif";
+import logo from "./imgs/header-logo.gif";
 
-const Header = () => {
+const Header = ({ loading }) => {
   return (
     <header className={styles.header}>
       <Link to="/">
-        <img src={loading} alt="" className={styles.loading} />
+        <img
+          src={loading ? loadingLogo : logo}
+          alt=""
+          className={styles.loading}
+        />
       </Link>
     </header>
   );
