b89bd044751f9de046c05e173613fa90246d7e87
Usman Suleiman <usmansbk@gmail.com>
2021-09-05 19:27:44 +0100

    Style board

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,7 +1,15 @@
 import styles from "./index.module.css";
 
 const Board = () => {
-  return <div className={styles.container}>Board</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.board}>
+          <ul className={styles.columns}></ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Board;
