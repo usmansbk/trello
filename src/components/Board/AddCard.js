ac875b22307a32a836e4df214f0281c971a45357
Usman Suleiman <usmansbk@gmail.com>
2021-09-05 22:18:58 +0100

    Set auto focus

--- src/components/Board/AddCard.js
+++ src/components/Board/AddCard.js
@@ -6,11 +6,13 @@ const AddCard = ({ onCancel }) => {
     <form className={styles.form}>
       <div className={styles.card}>
         <textarea
+          autoFocus
+          onBlur={onCancel}
           className={styles.textarea}
           placeholder="Enter a title for this card..."
         ></textarea>
       </div>
-      <div>
+      <div className={styles.actions}>
         <button className={styles.button}>Add card</button>
         <IconButton name="fa-times" onClick={onCancel} />
       </div>
