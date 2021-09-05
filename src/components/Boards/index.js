11cc5cf601f9bbd7aaa88ca984977acc78375859
Usman Suleiman <usmansbk@gmail.com>
2021-09-05 09:38:09 +0100

    Center boards

---
+++ src/components/Boards/index.js
@@ -0,0 +1,13 @@
+import styles from "./index.module.css";
+
+const Boards = () => {
+  return (
+    <div className={styles.container}>
+      <section className={styles.boards}>
+        <h1 className={styles.title}>YOUR BOARDS</h1>
+      </section>
+    </div>
+  );
+};
+
+export default Boards;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,26 @@
+import { Route, Switch, useRouteMatch } from "react-router";
+import Boards from "../Boards";
 import Header from "./Header";
 import styles from "./index.module.css";
 
+const routes = [
+  {
+    path: "",
+    component: Boards,
+  },
+];
+
 const Home = () => {
+  const { path: routePath } = useRouteMatch();
+
   return (
     <div className={styles.container}>
       <Header />
-      <h1>Home</h1>
+      <Switch>
+        {routes.map(({ path, component }) => (
+          <Route key={path} exact path={`${routePath}`} component={component} />
+        ))}
+      </Switch>
     </div>
   );
 };
