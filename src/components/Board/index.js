cd32d7a19236dbd22df37d4e173500594cd71422
Usman Suleiman <usmansbk@gmail.com>
2021-09-05 19:57:48 +0100

    Style columns

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,11 +1,37 @@
+import { nanoid } from "nanoid";
 import styles from "./index.module.css";
+
+const COLUMNS = [
+  {
+    id: nanoid(),
+    name: "Todo",
+  },
+  {
+    id: nanoid(),
+    name: "In progress",
+  },
+  {
+    id: nanoid(),
+    name: "Done",
+  },
+];
+
+const Column = ({ name }) => {
+  return <div>{name}</div>;
+};
 
 const Board = () => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.board}>
-          <ul className={styles.columns}></ul>
+          <ul className={styles.columns}>
+            {COLUMNS.map(({ name, id }) => (
+              <li key={id} className={styles.column}>
+                <Column name={name} />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
