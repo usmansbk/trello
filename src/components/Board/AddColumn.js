3dc547f22c9860da02fd21a8811e1dbede99477a
Usman Suleiman <usmansbk@gmail.com>
2021-09-06 09:50:07 +0100

    Container

--- src/components/Board/AddColumn.js
+++ src/components/Board/AddColumn.js
@@ -1,44 +1,48 @@
 import clsx from "clsx";
 import { useState } from "react";
+import CardButton from "../common/Button/CardButton";
 import IconButton from "../common/Button/IconButton";
 import Icon from "../common/Icon";
 import styles from "./AddColumn.module.css";
 
-const Input = () => {
+const Input = ({ onCancel }) => {
   return (
-    <form className={styles.form}>
-      <div className={styles.card}>
-        <input
-          autoFocus
-          className={styles.textarea}
-          placeholder="Enter list title..."
-        />
-      </div>
+    <form>
+      <input
+        autoFocus
+        className={styles.input}
+        placeholder="Enter list title..."
+      />
       <div className={styles.actions}>
-        <button type="submit" className={styles.button}>
-          Add list
-        </button>
-        <IconButton name="fa-times" />
+        <CardButton text="Add list" onClick={onCancel} />
+        <IconButton name="fa-times" onClick={onCancel} />
       </div>
     </form>
   );
 };
 
+const AddListButton = ({ onClick }) => (
+  <button className={styles.button} onClick={onClick}>
+    <Icon
+      name="fa-plus"
+      className={clsx(styles.buttonText, styles.buttonIcon)}
+    />
+    <span className={styles.buttonText}>Add another list</span>
+  </button>
+);
+
 const AddColumn = () => {
   const [edit, setEdit] = useState(false);
-  return !edit ? (
-    <button
-      className={clsx(styles.button, styles.container)}
-      onClick={() => setEdit(!edit)}
-    >
-      <Icon
-        name="fa-plus"
-        className={clsx(styles.buttonText, styles.buttonIcon)}
-      />
-      <span className={styles.buttonText}>Add another list</span>
-    </button>
-  ) : (
-    <Input />
+  const toggleEdit = () => setEdit(!edit);
+
+  return (
+    <div className={clsx(styles.container, edit && styles.card)}>
+      {!edit ? (
+        <AddListButton onClick={toggleEdit} />
+      ) : (
+        <Input onCancel={toggleEdit} />
+      )}
+    </div>
   );
 };
 
