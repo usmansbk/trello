31c78f11c96816d73671ee28efe15047d0b53a65
Usman Suleiman <usmansbk@gmail.com>
2021-09-07 20:03:21 +0100

    style modal title

--- src/components/Board/Details.js
+++ src/components/Board/Details.js
@@ -1,11 +1,17 @@
 import Modal from "../common/Modal";
-import styles from "./index.module.css";
+import styles from "./Details.module.css";
 
 const Details = ({ visible, onDismiss }) => {
   return (
     <Modal visible={visible} onDismiss={onDismiss} contentLabel="Task details">
       <div className={styles.container}>
-        <h1>Details</h1>
+        <header className={styles.header}>
+          <div className={styles.headerContent}>
+            <h2 className={styles.title}>
+              User should be able to input template image
+            </h2>
+          </div>
+        </header>
       </div>
     </Modal>
   );
