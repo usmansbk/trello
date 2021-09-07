1defc877a17e79dc6c0e0dd1ea09d51e72eefdc6
Usman Suleiman <usmansbk@gmail.com>
2021-09-07 20:14:46 +0100

    Style title

--- src/components/Board/Details.js
+++ src/components/Board/Details.js
@@ -1,16 +1,22 @@
 import Modal from "../common/Modal";
+import Icon from "../common/Icon";
 import styles from "./Details.module.css";
+
+const Title = ({ title }) => {
+  return (
+    <div className={styles.titleContainer}>
+      <h2 className={styles.title}>{title}</h2>
+    </div>
+  );
+};
 
 const Details = ({ visible, onDismiss }) => {
   return (
     <Modal visible={visible} onDismiss={onDismiss} contentLabel="Task details">
       <div className={styles.container}>
         <header className={styles.header}>
-          <div className={styles.headerContent}>
-            <h2 className={styles.title}>
-              User should be able to input template image
-            </h2>
-          </div>
+          <Icon name="fa-credit-card" className={styles.icon} />
+          <Title title="User should be able to input template image" />
         </header>
       </div>
     </Modal>
