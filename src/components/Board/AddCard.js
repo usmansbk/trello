4cf8d27709b9f83098b2ba83fcf98c3b48907d7d
Usman Suleiman <usmansbk@gmail.com>
2021-09-08 14:17:15 +0100

    Revert

--- src/components/Board/AddCard.js
+++ src/components/Board/AddCard.js
@@ -1,43 +1,26 @@
-import { nanoid } from "nanoid";
-import { useFormik } from "formik";
-import * as Yup from "yup";
+import { useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 import IconButton from "../common/Button/IconButton";
 import CardButton from "../common/Button/CardButton";
 import styles from "./AddCard.module.css";
 
 const AddCard = ({ onCancel }) => {
-  const formik = useFormik({
-    initialValues: {
-      title: "",
-    },
-    onSubmit: (values) => {
-      const data = {
-        id: nanoid(),
-        ...values,
-      };
-      console.log(data);
-    },
-    validationSchema: Yup.object({
-      title: Yup.string().max(512).required(),
-    }),
-  });
+  const [value, setValue] = useState("");
 
   return (
-    <form className={styles.form} onSubmit={formik.handleSubmit}>
+    <form className={styles.form} onSubmit={onCancel}>
       <div className={styles.card}>
         <TextareaAutosize
-          id="title"
-          name="title"
-          value={formik.values.title}
-          onChange={formik.handleChange}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
           autoFocus
+          onBlur={onCancel}
           className={styles.textarea}
           placeholder="Enter a title for this card..."
         />
       </div>
       <div className={styles.actions}>
-        <CardButton type="submit" text="Add card" />
+        <CardButton text="Add card" onClick={onCancel} />
         <IconButton name="fa-times" onClick={onCancel} />
       </div>
     </form>
