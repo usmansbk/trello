71a4b34bbf46fa62fc42b1570ae30c55fcaef485
Usman Suleiman <usmansbk@gmail.com>
2021-09-08 16:10:13 +0100

    Handle form data

--- src/components/Board/AddCard.js
+++ src/components/Board/AddCard.js
@@ -1,26 +1,38 @@
-import { useState } from "react";
+import { useCallback } from "react";
+import { useForm } from "react-hook-form";
 import TextareaAutosize from "react-textarea-autosize";
 import IconButton from "../common/Button/IconButton";
 import CardButton from "../common/Button/CardButton";
 import styles from "./AddCard.module.css";
 
 const AddCard = ({ onCancel }) => {
-  const [value, setValue] = useState("");
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = handleSubmit((data) => console.log(data));
+
+  const handleEnter = useCallback(
+    (e) => {
+      if (e.key === "Enter") {
+        onSubmit();
+      }
+      e.stopPropagation();
+    },
+    [onSubmit]
+  );
 
   return (
-    <form className={styles.form} onSubmit={onCancel}>
+    <form className={styles.form} onSubmit={onSubmit}>
       <div className={styles.card}>
         <TextareaAutosize
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
+          {...register("title", { required: true, maxLength: 512 })}
           autoFocus
-          onBlur={onCancel}
+          onKeyDown={handleEnter}
           className={styles.textarea}
           placeholder="Enter a title for this card..."
         />
       </div>
       <div className={styles.actions}>
-        <CardButton text="Add card" onClick={onCancel} />
+        <CardButton text="Add card" type="submit" />
         <IconButton name="fa-times" onClick={onCancel} />
       </div>
     </form>
