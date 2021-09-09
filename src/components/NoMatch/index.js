1edd2f9edae29b178e10b293eb3072265e47a80b
Usman Suleiman <usmansbk@gmail.com>
2021-09-09 10:02:45 +0100

    Add 404 page

--- src/components/NoMatch/index.js
+++ src/components/NoMatch/index.js
@@ -1,10 +1,14 @@
+import styles from "./index.module.css";
+
 const NoMatch = () => (
-  <section>
-    <h1>Malformed URL</h1>
-    <p>
-      The link you entered does not look like a valid Trello link. If someone
-      gave you this link, you may need to ask them to check that it's correct.
-    </p>
+  <section className={styles.container}>
+    <div className={styles.content}>
+      <h1 className={styles.title}>Malformed URL</h1>
+      <p className={styles.message}>
+        The link you entered does not look like a valid Trello link. If someone
+        gave you this link, you may need to ask them to check that it's correct.
+      </p>
+    </div>
   </section>
 );
 
