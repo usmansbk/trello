41a06ee7422e9096eab5edf281afc0693218d6e9
Usman Suleiman <usmansbk@gmail.com>
2021-09-09 09:00:28 +0100

    Add delete column logic

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,6 +2,12 @@
 
 export const CREATE_COLUMN = "board/create-column";
 export const CREATE_TASK = "column/create-task";
+export const DELETE_COLUMN = "column/delete";
+
+export const deleteColumn = (payload) => ({
+  type: DELETE_COLUMN,
+  payload,
+});
 
 export const createColumn = (payload) => ({
   type: CREATE_COLUMN,
